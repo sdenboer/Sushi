b52ee9834ad75997e585e278de44b680046bbb31
Sven den Boer <denboer.sven@gmail.com>
2022-04-04 18:15:50 +0200

    tmp output

--- SushiComponents/src/main/java/com/sushi/components/utils/Constants.java
+++ SushiComponents/src/main/java/com/sushi/components/utils/Constants.java
@@ -4,7 +4,7 @@ public class Constants {
 
     public static final long TRANSFER_MAX_SIZE = (1024 * 1024);
     public static final int BUFFER_SIZE = 2048;
-    public static final String TMP_DIR = "/home/pl00cc/tmp/output";
+    public static final String TMP_DIR = "/tmp/output";
     public static int DEFAULT_PORT = 9443;
     public static int TLS_PORT = 9444;
 
