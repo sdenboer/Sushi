9dba39615ff661c85f334bb264cbcf2bb9b500b1
Sven den Boer <denboer.sven@gmail.com>
2022-04-05 16:46:47 +0200

    cleaned up code

--- SushiClient/src/main/java/com/sushi/client/Client.java
+++ SushiClient/src/main/java/com/sushi/client/Client.java
@@ -3,12 +3,6 @@ package com.sushi.client;
 public class Client {
 
     public static void main(String[] args) {
-        args = new String[]{"--list", "-h localhost", "-p 9443", "-f /tmp/output"};
-
-        final String[] finalArgs = args;
-        for (int i = 0; i < 100; i++) {
-            new Thread(() -> CommandLineHandler.parse(finalArgs)).start();
-        }
-
+        CommandLineHandler.parse(args);
     }
 }
