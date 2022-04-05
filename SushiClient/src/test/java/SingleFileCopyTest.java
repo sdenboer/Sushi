bfa6b3fe87c2f15d85ccf46c99deb40561ed2d49
Sven den Boer <denboer.sven@gmail.com>
2022-04-05 16:47:48 +0200

    cleaned up code

--- SushiClient/src/test/java/SingleFileCopyTest.java
+++ SushiClient/src/test/java/SingleFileCopyTest.java
@@ -1,4 +1,4 @@
-import com.sushi.client.Client;
+import com.sushi.client.CommandLineHandler;
 import org.junit.Test;
 
 public class SingleFileCopyTest {
@@ -6,34 +6,31 @@ public class SingleFileCopyTest {
     @Test
     public void testPush() {
         String[] args = new String[]{"--backup", "-h localhost", "-p 9444", "-f /tmp/input/test.txt:/tmp/output"};
-        Client.main(args);
+        CommandLineHandler.parse(args);
     }
 
     @Test
     public void testPull() {
         String[] args = new String[]{"--fetch", "-h localhost", "-p 9444", "-f /tmp/input/test.txt"};
-        Client.main(args);
+        CommandLineHandler.parse(args);
     }
 
     @Test
     public void testFile() {
         String[] args = new String[]{"--list", "-h localhost", "-p 9443", "-f /tmp/output"};
-//        Client.main(args);
-        for (int i = 0; i < 1; i++) {
-            new Thread(() -> Client.main(args)).start();
-        }
+        CommandLineHandler.parse(args);
     }
 
     @Test
     public void testRemove() {
         String[] args = new String[]{"--remove", "-h localhost", "-p 9444", "-f /tmp/output/test.txt"};
-        Client.main(args);
+        CommandLineHandler.parse(args);
     }
 
     @Test
     public void testChecksum() {
         String[] args = new String[]{"--verify", "-h localhost", "-p 9443"};
-        Client.main(args);
+        CommandLineHandler.parse(args);
     }
 
 }
